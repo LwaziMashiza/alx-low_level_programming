0437c3cc03a8fc66ed06fe0afd84b64558d022c5
Mbuso <lwaziluthuli0@gmail.com>
2023-03-16 16:46:28 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-nclude<stdio.h>
+#include<stdio.h>
 /**puts Programme
  * *Code
  * */
 int main(void) 
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
