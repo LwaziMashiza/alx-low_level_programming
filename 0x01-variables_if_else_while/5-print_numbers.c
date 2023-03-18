3256c5821a91ddc77e674435fb213e392f9c4343
Mbuso <lwaziluthuli0@gmail.com>
2023-03-18 07:53:21 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * main - Prints the nunber from 0 to 10
+ * main - Prints the nunber from 0 to 9
  *
  * Return: Always 0 (Success)
  */
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		printf("%d", i);
 	}
