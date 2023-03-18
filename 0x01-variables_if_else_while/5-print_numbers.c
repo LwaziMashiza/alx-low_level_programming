f43f3e8f15261f8c3e3eeb49728d8865f0e132bf
Mbuso <lwaziluthuli0@gmail.com>
2023-03-18 07:28:42 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Prints the nunber from 0 to 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
