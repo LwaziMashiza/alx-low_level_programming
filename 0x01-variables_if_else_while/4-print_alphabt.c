d215d8d45a86dad8a92458789e8bf0d7799881d0
Mbuso <lwaziluthuli0@gmail.com>
2023-03-18 05:42:23 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets in lower then uppercase
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
