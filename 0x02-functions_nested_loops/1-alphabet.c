e285cb2d55c515c6e6b968b171af88bf74e00ad2
Mbuso <lwaziluthuli0@gmail.com>
2023-03-21 10:00:28 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <main.h>
+#include <unistd.h>
+
+/**
+ * main - Prints lower case alphabets by calling a function
+ * Return: 0 (Done)
+ **/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * Function - prints the alphabet in lower case
+ * Returns : void (Done)
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 0; i <= 'z' ; i++
+	{
+		_putchar(i);
+	}
+}
